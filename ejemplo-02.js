9dd3b3fde10cefdcbec430b736241ce011c9364b
Alvaro Delgado Monteagudo <delgadom@f-l3210-pc29.aulas.etsit.urjc.es>
2020-02-20 09:42:30 +0100

    ljlkj

--- ejemplo-02.js
+++ ejemplo-02.js
@@ -1,4 +1,18 @@
-const test = document.getElementById('test')
+console.log("Ejecutando JS...");
+// -- Acceder al párrafo de prueba
+const test = document.getElementById('test');
 
-console.log("Texto del parrafo: ")
-console.log(test.innerHTML);
+//-- Funcion de retrollamada
+test.onclick = () => {
+  console.log("Click!");
+
+  //-- Cambiar el color de fondo
+  //-- Si no tenia color asignado, poner amarillo
+  if (test.style.backgroundColor=="") {
+    test.style.backgroundColor = "yellow";
+
+  //-- Ya tiene color: quitarselo
+  } else {
+    test.style.backgroundColor = "";
+  }
+}
